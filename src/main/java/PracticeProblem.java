e48704bf39e29dfb34fcfba243e7a077a2fb267a
Jackie-Weng-ICS4U1-3-2025 <jackie.weng@student.tdsb.on.ca>
2025-04-07 21:46:13 +0000

    Completion

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,28 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void insertionSort(char[] characters) {
+        
+        for (int i = 1; i < characters.length; i++) {
+            char key = characters[i]; 
+            int index = i - 1;
 
-	}
+     
+            while (index >= 0 && characters[index] > key) {
+                characters[index + 1] = characters[index];
+                index--;
+            }
+            characters[index + 1] = key; 
+        }
 
+        
+        for (int i = 0; i < characters.length; i++) {
+            System.out.print(characters[i] + " ");
+        }
+        System.out.println();
+    }
 
+    public static void main(String[] args) {
+        char[] testArray = {'g', 'e', 'b', 'a', 'd', 'f', 'c'};
+        insertionSort(testArray);
+    }
 }
